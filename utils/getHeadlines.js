e5f1b9e29ceebec55a63978aa85aa40b1dcab5ae
herseus <707678956@qq.com>
2025-08-31 19:39:37 +0800

    Update getHeadlines.js

--- utils/getHeadlines.js
+++ utils/getHeadlines.js
@@ -1,25 +1,33 @@
 const axios = require('axios');
 const timeago = require('timeago.js');
+function formatTimestamp(timestamp) {
+    const date = new Date(timestamp * 1000);
+
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // 月份从 0 开始
+    const day = String(date.getDate()).padStart(2, '0');
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+}
 
 const getHeadlines = async (date) => {
   console.log('start fetching headlines')
   try {
-    // end of the date
-    const endTime = Math.round(new Date(date).getTime() / 1000);
-    // 1 hour before start of the date (save missed posts)
-    const startTime = Math.round(new Date(date).getTime() / 1000) - (25 * 60 * 60);
     const res = await axios.get(`https://www.reddit.com/r/books/hot.json?limit=10&t=day`, {
         headers: {
-            'User-Agent': "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Mobile Safari/537.36",
+            'User-Agent': "hackernews-daily:bot:v1 (by /u/herseus)",
         }
     });
-    const top10Objs = response.data.data.children(-10);
+    const top10Objs = res.data.data.children.slice(-10);
     // console.log(top10Objs)
     const contents = top10Objs
       .map((obj, i) => {
-        let { title, created, selftext, url, author, ups, num_comments } = obj;
+        let { title, created, selftext, url, author, ups, num_comments } = obj.data;
         return `${i + 1}. **[${title}](${url})**
-${ups} ups by ${author} ${timeago.format(created)} | [${num_comments} comments](${url})
+${ups} ups by ${author} ${formatTimestamp(created)} | [${num_comments} comments](${url})
 
 `;
       })
